85b9ff15c769af4727b9f3f7b80148a546115bcf
Tim Norman <tim@timnorman.ca>
2023-11-12 20:40:15 -0500

    Due date also sets due week.

--- src/main/java/ca/timnorman/semesterscheduler/CourseItem.java
+++ src/main/java/ca/timnorman/semesterscheduler/CourseItem.java
@@ -1,11 +1,13 @@
 package ca.timnorman.semesterscheduler;
 
+import java.time.ZoneId;
+import java.time.temporal.ChronoField;
 import java.util.Calendar;
 import java.util.Date;
 
 public class CourseItem {
     String description; // Required
-    int weekDueDate;    // Required
+    int weekDue;    // Required
     Calendar dueDate;   // Optional
     int gradeWeight;    // Optional
 
@@ -17,12 +19,12 @@ public class CourseItem {
         this.description = description;
     }
 
-    public int getWeekDueDate() {
-        return weekDueDate;
+    public int getWeekDue() {
+        return weekDue;
     }
 
-    public void setWeekDueDate(int weekDueDate) {
-        this.weekDueDate = weekDueDate;
+    public void setWeekDue(int weekDue) {
+        this.weekDue = weekDue;
     }
 
     public Calendar getDueDate() {
@@ -31,6 +33,7 @@ public class CourseItem {
 
     public void setDueDate(Calendar dueDate) {
         this.dueDate = dueDate;
+        setWeekDue(calculateWeekDue(dueDate));
     }
 
     public int getGradeWeight() {
@@ -51,11 +54,36 @@ public class CourseItem {
         }
         return "CourseItem{" +
                 "description='" + description + '\'' +
-                ", weekDueDate=" + weekDueDate +
+                ", weekDue=" + weekDue +
                 ", dueDate=" + shortDate +
                 ", gradeWeight=" + gradeWeight +
                 '}';
     }
 
+    /**
+     * Calculate the semester week number an item is due based on the due date
+     * @param dueDate The due date
+     * @return The week number the item is due relative to the semester start date.
+     */
+    private int calculateWeekDue(Calendar dueDate) {
+        int startingWeek = weekNumberOfCalendarDate(Schedule.startDate) - 1;
+        int weekItsDue = weekNumberOfCalendarDate(dueDate);
+        return weekItsDue - startingWeek;
+
+    }
+
+    /**
+     * Returns the week number of the year given a date.
+     * @param date Date object as a Calendar object.
+     * @return Week number as in int.
+     */
+    private int weekNumberOfCalendarDate(Calendar date) {
+        return date
+                .getTime()
+                .toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate()
+                .get(ChronoField.ALIGNED_WEEK_OF_YEAR);
+    }
 
 }
